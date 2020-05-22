5afebfc0bed5e301a88db140b4e7c1778ccb2386
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-22 08:50:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(str)
+  array = str.split(" ")
+  array_1 = []
+  array.each { |element|
+    array_1 << element.reverse
+  }
+  array_1.join(" ")
+end 
+
+def reverse_each_word(str)
+  array = str.split(" ")
+  array.collect { |element|
+  element.reverse!
+  }
+  array.join(" ")
+end 
